5fb18e26396b0a1800ec7feba80be17a374de727
Steve Bauman <steven_bauman@outlook.com>
2024-09-24 11:44:18 -0400

    Add more tests

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -269,3 +269,39 @@ it('can be re-retrieved with fresh', function () {
     expect($fresh->is($model))->toBeTrue();
     expect($fresh->name)->toBe('John Doe');
 });
+
+it('can get all results', function () {
+    foreach (range(1, 20) as $index) {
+        ModelStub::create();
+    }
+
+    expect(ModelStub::get())->toHaveCount(20);
+});
+
+it('can chunk using each on results', function () {
+    foreach (range(1, 20) as $index) {
+        ModelStub::create();
+    }
+
+    $count = 0;
+
+    ModelStub::each(function ($model) use (&$count) {
+        expect($model)->toBeInstanceOf(ModelStub::class);
+
+        $count++;
+    });
+
+    expect($count)->toBe(20);
+});
+
+it('can chunk chunk results', function () {
+    foreach (range(1, 20) as $index) {
+        ModelStub::create();
+    }
+
+    ModelStub::chunk(10, function ($models) {
+        // Redis does not guarantee the count, so we just
+        // check if it's greater than or equal to 10.
+        expect($models->count())->toBeGreaterThanOrEqual(10);
+    });
+});
